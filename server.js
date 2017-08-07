39d3daf229917b42d2ba0eba39a2e4b9c5e6eff3
Nick Tamburro <Nick Tamburro>
2017-08-06 22:31:38 -0700

    added mlab connection(hopefully)

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static("public"));
 // -------------------------------------------------
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("mongodb://localhost:27017");
+var databaseURI = "mongodb://localhost:27017";
+
 var db = mongoose.connection;
 db.on("error", function(err) {
   console.log("Mongoose Error: ", err);
@@ -26,6 +27,12 @@ db.on("error", function(err) {
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
+
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGODB_URI);
+}else{
+  mongoose.connect(databaseURI);
+}
 // -------------------------------------------------
 // Main "/" Route. This will redirect the user to our rendered React application
 app.get("/", function(req, res) {
